ba97f0766963b6d053866acaa22f45d7dd06fda1
ichathux <ichathux@gmail.com>
2023-07-23 15:37:48 +0530

    api request created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthContentComponent } from './auth-content/auth-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthContentComponent
   ],
   imports: [
     BrowserModule,
